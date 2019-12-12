1576046c37c9b89864794509b68087901a556f00
Aminah Nuraini <aminah.nuraini92@gmail.com>
2019-12-12 07:57:06 +0700

    Update middleware.py

--- scrapy_fake_useragent/middleware.py
+++ scrapy_fake_useragent/middleware.py
@@ -1,6 +1,10 @@
 import logging
 from fake_useragent import UserAgent
 
+from scrapy import signals
+from scrapy.downloadermiddlewares.retry import *
+from scrapy.spidermiddlewares.httperror import *
+
 logger = logging.getLogger(__name__)
 
 class RandomUserAgentMiddleware(object):
@@ -31,3 +35,32 @@ class RandomUserAgentMiddleware(object):
             request.headers.setdefault('User-Agent', self.proxy2ua[proxy])
         else:
             request.headers.setdefault('User-Agent', get_ua())
+            
+class RetryUserAgentMiddleware(RetryMiddleware):
+
+    def __init__(self, settings):
+        super(RetryUserAgentMiddleware, self).__init__(settings)
+
+        fallback = settings.get('FAKEUSERAGENT_FALLBACK', None)
+        self.ua = UserAgent(fallback=fallback)
+        self.ua_type = settings.get('RANDOM_UA_TYPE', 'random')
+
+    def get_ua(self):
+        '''Gets random UA based on the type setting (random, firefox…)'''
+        return getattr(self.ua, self.ua_type)
+
+    def process_response(self, request, response, spider):
+        if request.meta.get('dont_retry', False):
+            return response
+        if response.status in self.retry_http_codes:
+            reason = response_status_message(response.status)
+            request.headers['User-Agent'] = self.get_ua()
+            return self._retry(request, reason, spider) or response
+        return response
+        
+    def process_exception(self, request, exception, spider):
+        if isinstance(exception, self.EXCEPTIONS_TO_RETRY) \
+                and not request.meta.get('dont_retry', False):
+            request.headers['User-Agent'] = self.get_ua()
+            return self._retry(request, exception, spider)
+
